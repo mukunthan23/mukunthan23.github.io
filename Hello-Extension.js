f4a11e0c26237ae635bfa382190db5bd07e5d58c
mukunthan23 <48052297+mukunthan23@users.noreply.github.com>
2019-02-27 19:53:37 +0800

    Update Hello-Extension.js

--- Hello-Extension.js
+++ Hello-Extension.js
@@ -1,12 +1,8 @@
-
-
 $(document).ready(function() {
 
   // Hook up an event handler for the load button click.
   // Wait to initialize until the button is clicked.
   $("#initializeButton").click(function() {
-	  
-
 
     // Disable the button after it's been clicked
     $("#initializeButton").prop('disabled', true);
@@ -18,36 +14,10 @@ $(document).ready(function() {
 
       // Display the name of dashboard in the UI
       $("#resultBox").html("I'm running in a dashboard named <strong>" + dashboard.name + "</strong>");
-	  
-	  const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
-
-    // Find a specific worksheet 
-    /*var worksheet = worksheets.find(function (sheet) {
-      return sheet.name === "Extension Testing";
-    });
-$("#resultBox1").html("I'm running in a dashboard named <strong>" + dashboard.name + "</strong>");*/
-	    
-	    /*
-	// get the summary data for the sheet
- worksheet.getSummaryDataAsync().then(function (sumdata) {
-
-  const worksheetData = sumdata;
-  // The getSummaryDataAsync() method returns a DataTable
-  // Map the DataTable (worksheetData) into a format for display, etc.
-  $("#resultBox1").html("I'm running in a dashboard named <strong>" + dashboard.name + "</strong>");
-  
- });
-  */
-
-
     }, function(err) {
 
       // something went wrong in initialization
       $("#resultBox").html("Error while Initializing: " + err.toString());
     });
   });
-  
-  
 });
-
-
