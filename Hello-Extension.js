db3f4bffdaa9039a0bd6e29e4beda665b53a3f79
mukunthan23 <48052297+mukunthan23@users.noreply.github.com>
2019-02-27 20:08:16 +0800

    Update Hello-Extension.js

--- Hello-Extension.js
+++ Hello-Extension.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   // Hook up an event handler for the load button click.
   // Wait to initialize until the button is clicked.
-  $("#initializeButton").click(function() {
+
 
     // Disable the button after it's been clicked
     $("#initializeButton").prop('disabled', true);
@@ -19,5 +19,5 @@ $(document).ready(function() {
       // something went wrong in initialization
       $("#resultBox").html("Error while Initializing: " + err.toString());
     });
-  });
+
 });
