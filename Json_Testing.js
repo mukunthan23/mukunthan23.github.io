90c93f945a611aedc205d60dae4ad1059f6fce11
mukunthan23 <48052297+mukunthan23@users.noreply.github.com>
2019-03-01 12:19:23 +0800

    Update Json_Testing.js

--- Json_Testing.js
+++ Json_Testing.js
@@ -28,8 +28,8 @@ $(document).ready(function() {
 			for (var i=0; i < worksheetData.length; i++) {
 			labels.push(worksheetData[i][0].formattedValue);
 			data.push(worksheetData[i][1].value);
-		        alert(worksheetData[i][0].formattedValue);
-			alert(worksheetData[i][1].value);
+		        //alert(worksheetData[i][0].formattedValue);
+			//alert(worksheetData[i][1].value);
 			}
 
 			$("#resultBox").html(JSON.stringify(output));
