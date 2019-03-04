8d3aab78373bc9c998b8a6499d172d3db31de892
mukunthan23 <48052297+mukunthan23@users.noreply.github.com>
2019-03-04 11:18:15 +0800

    Update Json_Testing.js

--- Json_Testing.js
+++ Json_Testing.js
@@ -230,7 +230,7 @@ $(document).ready(function() {
 				var fieldname="Category 1";
 				var fieldvalue=d.name;
 				//worksheet1.clearFilterAsync(fieldname);
-				worksheet1.selectMarksAsync(fieldname.toString(),fieldvalue.toString(),tableau.FilterUpdateType.REMOVE);
+				worksheet1.selectMarksAsync(fieldname,fieldvalue,tableau.SelectionUpdateType.REPLACE);
 				}
 				catch(err) {
 					  alert(err.message);
